37eb3e273c84f1aea46459a52cb819e3ee027ca9
Dennis Dang <dennisdang80@yahoo.com>
2023-09-01 23:51:40 -0500

    Finished opening animation, added about html file

--- app.js
+++ app.js
@@ -1,31 +1,19 @@
 let intro = document.querySelector('.intro');
 let logo = document.querySelector('.logo-header');
-let logoSpan = document.querySelectorAll('.logo');
-let rectangleSpan = document.querySelectorAll('.rectangle');
+let logoWords = document.querySelector('.logo');
+let rectangle = document.querySelector('.rectangle');
 
 window.addEventListener('DOMContentLoaded', ()=>{
     setTimeout(()=>{
-        logoSpan.forEach((span, idx)=>{
+        logoWords.classList.add('active');
+        setTimeout(()=>{
+            rectangle.classList.add('active');
             setTimeout(()=>{
-                span.classList.add('active');
-            }, (idx + 1) * 400)
-        })
+                intro.classList.add('fade');
+                setTimeout(()=>{
+                    intro.remove();
+                }, 500)
+            }, 2900)
+        }, 1000)
     })
-
-    setTimeout(()=>{
-        rectangleSpan.forEach((span, idx)=>{
-            setTimeout(()=>{
-                span.classList.add('active');
-            }, (idx + 1) * 400)
-        })
-    }, 1000)
-
-    setTimeout(()=>{
-        intro.style.opacity = '0';
-    }, 4000)
-
-    setTimeout(()=>{
-        intro.style.opacity = '0';
-        intro.style.display = 'none';
-    }, 5300)
 })
